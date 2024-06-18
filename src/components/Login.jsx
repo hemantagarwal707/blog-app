b131e3d44ab7a289419ef3723fa34bba21ef5e01
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 19:04:55 +0530

    login page design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    
+    <div className="container">
+    <div className=' d-flex justify-content-center align-items-center' style={{height: '100vh'}} >
+<img src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="Google Login" style={{width:'10%'}} />
+
+
+
+</div>
+
+</div>
+    
+    
+    
+    </>
   )
 }
 
