5109576aab89cdf09e61070ea66ba94fd99b293b
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 23:00:00 +0530

    adding google authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { auth } from '../Firebase'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 const Login = () => {
+
+
+    const signInWithGoogle = async () => {
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+
+
+
+    }
+
   return (
     <>
     
     <div className="container">
     <div className=' d-flex justify-content-center align-items-center' style={{height: '100vh'}} >
-<img src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="Google Login" style={{width:'10%'}} />
+<img
+onClick={signInWithGoogle}
+ src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="Google Login" style={{width:'10%'}} />
 
 
 
