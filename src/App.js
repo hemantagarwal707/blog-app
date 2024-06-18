2dc40f8499f1a88c8abae47e332c676594b8813f
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 23:05:44 +0530

    register all routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Login from './components/Login'
+import Blogs from './components/Blogs'
+import SingleBlog from './components/SingleBlog'
+import AddBlog from './components/AddBlog'
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
    <>
-   <Login />
    
+   <Router>
+   <Routes>
+     <Route path='/' element={<Login />} />
+     <Route path='/blogs' element={<Blogs />} />
+     <Route path='/addblog' element={<AddBlog />} />
+     <Route path='/blogs/:id' element={<SingleBlog />} />
+   </Routes>
+  </Router>
    
    
    </>
