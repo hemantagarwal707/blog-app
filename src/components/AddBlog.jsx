214c9734ecdd4a13bd9fb3268548a4355bc8a0bb
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-19 13:15:51 +0530

    form input handling

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,43 +1,82 @@
-import React from 'react'
+import React , { useState } from 'react'
 import Navbar from './Navbar'
 
 const AddBlog = () => {
+
+
+  
+  const [data, setData] = useState({
+      title: '',
+      shortDesc: '',
+      longDesc: '',
+      imgUrl: '',
+      
+  }) 
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+}
   return (
   <>
   <Navbar/>
   <div className="container my-3" style={{ padding: '2rem', width: '50%' }}>
   <form>
-      <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Title</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-
-      
-      <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Short Desciption</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-
   <div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Full Desciption</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <label for="exampleInputEmail1" class="form-label">Title</label>
+  <input
+      value={data.title}
+      name="title"
+      type="text"
+      required
+      onChange={handleChange}
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp" />
+
 </div>
 
-
+<div class="mb-3">
+  <label for="text" class="form-label">Short Description</label>
+  <input
+      value={data.shortDesc}
+      name="shortDesc"
+      type="text"
+      required
+      onChange={handleChange}
+      class="form-control"
+      id="exampleInputPassword1" />
+</div>
 
 <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Image URL</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
+  <label for="exampleFormControlTextarea1" class="form-label">Full Desciption</label>
+  <textarea
+      value={data.logDesc}
+      name="longDesc"
+      required
+      onChange={handleChange}
+      class="form-control"
+      id="exampleFormControlTextarea1"
+      rows="3"
+  ></textarea>
+</div>
 
+<div class="mb-3">
+  <label for="exampleInputPassword1" class="form-label">imge Url</label>
+  <input
+      value={data.imgUrl}
+      name="imgUrl"
+      type="text"
+      required
+      onChange={handleChange}
+      class="form-control"
+      id="exampleInputPassword1"
+  />
+</div>
 
-    
+<div className='mb-3'>
 
+  <button type="submit" class="btn btn-primary btn-lg">Add Blog</button>
+</div>
 
-          <button type="submit" class="btn btn-primary ">Add Blog</button>
      
 
   </form>
