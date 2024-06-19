af5c42bdeaccd529e47cc960c3316a12b179840e
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-19 17:47:00 +0530

    login button design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { auth } from '../Firebase'
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
-
+import './Login.css'
 const Login = () => {
   const navigate = useNavigate();
 
@@ -18,13 +18,18 @@ const Login = () => {
   return (
     <>
     
-    <div className="container">
-    <div className=' d-flex justify-content-center align-items-center' style={{height: '100vh'}} >
-<img
-onClick={signInWithGoogle}
- src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="Google Login" style={{width:'10%'}} />
+    <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", gap: "5px", height: "100vh" }}>
+    {/* <img src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="login-with-google" style={{ width: "5%" }} />
+    <button style={{ padding: "1.5rem", fontSize: "1.5rem", fontFamily: "sans-serif", fontWeight: "bold", color: "red", borderRadius: "5%" }}
+        onClick={signInWithGoogle}
+    >
 
-
+        Login with Google</button> */}
+<div class="login-container">
+        <button class="login-button" onClick={signInWithGoogle}>
+<img src="https://static.vecteezy.com/system/resources/previews/012/871/371/original/google-search-icon-google-product-illustration-free-png.png" alt="Google Logo" class="google-logo" />
+Login with Google
+</button>
 
 </div>
 
